086514e3223ff41dde3f3a2cf5e4732770c4a915
Mehar Fatma <mehar.fatma202@gmail.com>
2024-07-10 19:05:06 +0530

    working on JS logic

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 let box = document.querySelectorAll("button");
 
-let marker = "O";
+let marker = ""; //x,
 
 box.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        button.style.color = "white";
-        button.style.fontSize = "40px";
-        console.log(button.innerText = marker);
-        // console.log(button);
-    });
-});
+  const handler = () => {
+    if (marker === "") {
+      button.style.color = "brown";
+      button.style.fontSize = "40px";
 
+      marker = button.innerText = "X";
+      console.log(marker);
+    } else if (marker === "O") {
+      button.style.color = "brown";
+      button.style.fontSize = "40px";
+
+      marker = button.innerText = "X";
+      console.log(marker);
+    } else if (marker === "X") {
+      button.style.color = "white";
+      button.style.fontSize = "40px";
+
+      marker = button.innerText = "O";
+      console.log(marker);
+    }
+  };
+
+  button.addEventListener("click", handler);
+
+  // button.removeEventListener("click", handler);
+});
