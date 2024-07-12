5a4d542b2d5de1a0d4215928882aaaa4ff057911
Mehar Fatma <mehar.fatma202@gmail.com>
2024-07-12 17:15:53 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let box = document.querySelectorAll("button");
 
+let btnIdx = 0;
+
 let marker = "O";
 
 const styles = {
@@ -9,13 +11,14 @@ const styles = {
 
 box.forEach((button) => {
   const handler = () => {
+    btnIdx += 1;
     const nextMarker = marker === "O" ? "X" : "O";
     const { color, fontSize } = styles[marker];
 
     button.style.color = color;
     button.style.fontSize = fontSize;
     marker = button.innerText = nextMarker;
-    console.log(marker);
+    console.log(btnIdx);
   };
 
   button.addEventListener("click", handler, { once: true });
