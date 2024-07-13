e2cb7872be095c17dad4ec7d96e0c310e3bfa4e1
Mehar Fatma <mehar.fatma202@gmail.com>
2024-07-14 01:08:26 +0530

    additional changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let box = document.querySelectorAll("button");
+let mainBox = document.querySelector(".main-box");
+// console.dir(mainBox);
 
-let btnIdx = 0;
-
+let boxCount = 0;
 let marker = "O";
 
 const styles = {
@@ -10,16 +11,34 @@ const styles = {
 };
 
 box.forEach((button) => {
+  boxCount += 1;
   const handler = () => {
-    btnIdx += 1;
     const nextMarker = marker === "O" ? "X" : "O";
     const { color, fontSize } = styles[marker];
 
     button.style.color = color;
     button.style.fontSize = fontSize;
     marker = button.innerText = nextMarker;
-    console.log(btnIdx);
   };
-
+  console.log(boxCount);
   button.addEventListener("click", handler, { once: true });
 });
+
+const buttonIndexHandler = (e) => {
+  const clickedButton = e.target;
+  const children = Array.from(mainBox.children);
+  const index = children.indexOf(clickedButton);
+  console.log("Clicked element index:", index);
+};
+mainBox.addEventListener("click", buttonIndexHandler);
+
+const winningPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
